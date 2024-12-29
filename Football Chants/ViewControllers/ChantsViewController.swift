0ba6b71bce8513b656423d09c50e7c6bb4740b46
josymarss <sylvajosymar@gmail.com>
2024-12-29 23:45:53 +0100

    done

--- Football Chants/ViewControllers/ChantsViewController.swift
+++ Football Chants/ViewControllers/ChantsViewController.swift
@@ -20,6 +20,12 @@ class ChantsViewController: UIViewController {
         return tv
     }()
     
+    // MARK: Lifecycle
+    override func loadView() {
+        super.loadView()
+        setUp()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBlue
@@ -27,4 +33,44 @@ class ChantsViewController: UIViewController {
     }
     
 
+}
+
+private extension ChantsViewController {
+    func setUp() {
+        
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+
+extension ChantsViewController: UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        switch indexPath.row {
+            case 0 :
+                cell.backgroundColor = .systemRed
+            case 1:
+                cell.backgroundColor = .systemCyan
+            case 3:
+                cell.backgroundColor = .systemPink
+            default :
+                break
+        }
+        
+        return cell
+    }
 }
